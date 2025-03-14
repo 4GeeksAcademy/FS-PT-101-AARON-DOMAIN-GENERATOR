49b1c169b7215a7b21e688b4d1f4872a570e1a26
aaron2210 <64928964+aaron2210@users.noreply.github.com>
2025-03-14 20:27:05 +0000

    Generador de dominios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let dom = ['es', 'com', 'io', 'org', 'net', 'us'];
+
+  let dominios_generados = document.getElementById("dominios_generados");
+
+  pronoun.forEach(i=> adj.forEach(j=> noun.forEach(k=> dom.forEach(z=>{
+    let dominio = `${i}${j}${k}.${z}`;
+    let li=document.createElement("li");
+    li.textContent=dominio;
+    dominios_generados.appendChild(li);
+  }))));
+
+  pronoun.forEach(i=>adj.forEach(j=>noun.forEach(k=>dom.forEach(z=>console.log(i+j+k+z)))));
 };
